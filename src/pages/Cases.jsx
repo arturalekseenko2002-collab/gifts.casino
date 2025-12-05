78c0bc8e702d34b43d9dfef6d13914eba98d148e
НовыйИмя <Email>
2025-12-05 15:23:45 +0100

    d

--- src/pages/Cases.jsx
+++ src/pages/Cases.jsx
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import "../styles/Cases.css";
-import { useUser } from "../UserContext";
 
 const Cases = () => {
-    return (
+  // ВРЕМЕННЫЕ заглушки, пока нет контекста и бэка
+  const user = null;
+  const userLoading = false;
+  const initials = "";
+  const displayName = "";
+  const displayUsername = "";
+
+  return (
     <div className="App">
-        <div className="Main_Window">   
-            <div className="mainHomePageContainer">  
-                <div className="headerContainer">
-                    <div className="circleInHeaderContainer">
-                        {user?.photoUrl ? (
-                            <img src={user.photoUrl} className="userAvatar"/>
-                        ) : (
-                            <div className="circleName">
-                                {userLoading ? "" : initials}
-                            </div>
-                        )}
-                    </div>
-                    <div className="nickNameContainer">
-                        <div className="nickNameContainerPart1">{userLoading ? "Загрузка" : displayName}</div>
-                        <div className="nickNameContainerPart2">{userLoading ? "" : displayUsername}</div>
-                    </div>
+      <div className="Main_Window">
+        <div className="mainHomePageContainer">
+          <div className="headerContainer">
+            <div className="circleInHeaderContainer">
+              {user?.photoUrl ? (
+                <img src={user.photoUrl} className="userAvatar" />
+              ) : (
+                <div className="circleName">
+                  {userLoading ? "" : initials}
                 </div>
+              )}
             </div>
+            <div className="nickNameContainer">
+              <div className="nickNameContainerPart1">
+                {userLoading ? "Загрузка" : displayName}
+              </div>
+              <div className="nickNameContainerPart2">
+                {userLoading ? "" : displayUsername}
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-)};
+  );
+};
 
 export default Cases;
