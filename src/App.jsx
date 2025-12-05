f23e9f04717316661f72d3d02786d0d88c74eab9
НовыйИмя <Email>
2025-12-05 15:04:41 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Main";
+import Cases from "./pages/Cases";
 import Withdraw from "./pages/Withdraw";
 
 const App = () => {
